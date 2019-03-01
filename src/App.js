f61546100d998716346fa4225f7abe1f433ca1eb
Javier <jcoucelanga@gmail.com>
2019-03-01 15:30:33 +0100

    all methods implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import BarChart from './BarChart';
 import WorldMap from './WorldMap';
 import { scaleThreshold } from 'd3-scale'
+import dataFeed from './resources/worldmap-dataexample.json';
 
 const colorScale = scaleThreshold().domain([5,10,20,30]).range(["#75739F", "#5EAFC6", "#41A368", "#93C464"])
 
 class App extends Component {
   constructor(props){
     super(props)
-    this.onHover = this.onHover.bind(this)
-    this.state = { screenWidth: 1000, screenHeight: 500, hover: "none", brushExtent: [0,40] }
+    this.state = { screenWidth: 1000, screenHeight: 500, hover: "none", brushExtent: [0,40], data: dataFeed }
 
   }
 
-  onHover(d) {
+  onHover = (d) => {
     this.setState({ hover: d.id })
-    console.log('holaaa');
   }
 
   render() {
@@ -28,8 +25,8 @@ class App extends Component {
           </h2>
         </div>
         <div className={'bar'}>
-
-        <WorldMap hoverElement={this.state.hover} onHover={this.onHover} colorScale={colorScale} />
+        <ul>{this.state.data.map((element, i) => <li key={i}>{element.label}</li>)}</ul>
+        <WorldMap data={this.state.data} hoverElement={this.state.hover} onHover={this.onHover} colorScale={colorScale} />
         </div>
       </div>
     )
--- src/WorldMap.js
+++ src/WorldMap.js
@@ -3,20 +3,52 @@ import './App.css'
 import worlddata from './world'
 import { geoMercator, geoPath } from 'd3-geo'
 
+const getCountryISO3 = require("country-iso-2-to-3");
+const mapWorldData = (countries, data) => {
+    var filteredData = countries;
+    data.forEach(dato => {
+        countries.forEach((country, i) => {
+            if (country.id === dato.segment) {
+                filteredData.splice(i, 1, { ...country, level: dato.level, puntos: dato.nb_uniq_actions_searchkeyword });
+            }
+        })
+    })
+    return filteredData;
+}
+const parseMapData = (data) => {
+    let parsedData = data.map(item => {
+        let itemId = item.segment.replace('countryCode==', '');
+        item.segment = getCountryISO3(itemId.toUpperCase());
+        return { ...item }
+    })
+    return parsedData;
+}
 class WorldMap extends Component {
+    componentDidMount() {
+    }
     render() {
+        let finalData = parseMapData(this.props.data);
+        let finalMap = mapWorldData(worlddata.features, finalData);
         const projection = geoMercator().scale(120)
             .translate([430, 250])
         const pathGenerator = geoPath().projection(projection)
-        const countries = worlddata.features
+        console.log('-----');
+        console.log(worlddata.features);
+        console.log(finalMap);
+        console.log(getCountryISO3("ES"));
+        console.log('-----');
+        const countries = finalMap
             .map((d, i) => <path
                 key={'path' + i}
                 d={pathGenerator(d)}
-                onMouseEnter={() => { this.props.onHover(d) }}
-                style={{ fill: this.props.hoverElement === d.id ? "#FCBC34" : this.props.colorScale(d.launchday), stroke: "black", strokeOpacity: 0.5 }}
+                //onMouseEnter={() => { this.props.onHover(d) }}
+                style={{
+                    fill: d.puntos > 5 ? "#FCBC34" : this.props.colorScale(d.launchday),
+                    stroke: "black", strokeOpacity: 0.5,
+                }}
                 className='countries'
             />)
-        return <svg width={'50%'} height={750}>
+        return <svg height={750}>
             {countries}
         </svg>
     }
